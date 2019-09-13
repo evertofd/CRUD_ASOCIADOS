16f1d20b59f05a6390d0dba7358fa934c5bd2e87
everto <everto.fariasd@gmail.com>
2019-09-12 21:29:33 -0300

    Incluyendo Bootstrap

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,6 +17,6 @@ end
 
   private
   def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :email)
+    params.require(:employee).permit(:first_name, :last_name, :email, :area_id)
   end
 end
